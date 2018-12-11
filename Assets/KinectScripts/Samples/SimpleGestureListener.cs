a8c412f108ddeb8885bd954c51bd86c0f5fa8fca
vluhadia <32604792+vluhadia@users.noreply.github.com>
2018-12-11 17:31:50 -0500

    Revert "initial ending positions"
    This reverts commit 4806142e78ddc3c383cab305693912223bf87777.

--- Assets/KinectScripts/Samples/SimpleGestureListener.cs
+++ Assets/KinectScripts/Samples/SimpleGestureListener.cs
@@ -32,89 +32,85 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
 		// as an example - detect these user specific gestures
 		KinectManager manager = KinectManager.Instance;
 
-		manager.DetectGesture(userId, KinectGestures.Gestures.Jump);
-		manager.DetectGesture(userId, KinectGestures.Gestures.Squat);
-
-//		manager.DetectGesture(userId, KinectGestures.Gestures.Push);
-//		manager.DetectGesture(userId, KinectGestures.Gestures.Pull);
-
-//		manager.DetectGesture(userId, KinectWrapper.Gestures.SwipeUp);
-//		manager.DetectGesture(userId, KinectWrapper.Gestures.SwipeDown);
-
-		if(gestureInfo != null)
+		if (gestureInfo != null)
 		{
-			gestureInfo.text = "SwipeLeft, SwipeRight, Jump or Squat.";
+			gestureInfo.text = "Capture planets by clicking on them!";
 		}
 	}
 
 	public void UserLost(uint userId, int userIndex)
 	{
-		if(gestureInfo != null)
+		if (gestureInfo != null)
 		{
 			gestureInfo.text = string.Empty;
 		}
 	}
 
 	public void GestureInProgress(uint userId, int userIndex, KinectGestures.Gestures gesture,
-	                              float progress, KinectWrapper.NuiSkeletonPositionIndex joint, Vector3 screenPos)
+		float progress, KinectWrapper.NuiSkeletonPositionIndex joint, Vector3 screenPos)
 	{
-		if(gesture == KinectGestures.Gestures.Click && progress > 0.3f)
+		if (gesture == KinectGestures.Gestures.Click && progress > 0.3f)
 		{
-			string sGestureText = string.Format ("{0} {1:F1}% complete", gesture, progress * 100);
-			if(gestureInfo != null)
-				gestureInfo.text = sGestureText;
+			string sGestureText = string.Format("capture {0:F1}% complete", progress * 100);
 
-			progressDisplayed = true;
-		}
-		else if((gesture == KinectGestures.Gestures.ZoomOut || gesture == KinectGestures.Gestures.ZoomIn) && progress > 0.5f)
-		{
-			string sGestureText = string.Format ("{0} detected, zoom={1:F1}%", gesture, screenPos.z * 100);
-			if(gestureInfo != null)
-				gestureInfo.text = sGestureText;
-
-			progressDisplayed = true;
-		}
-		else if(gesture == KinectGestures.Gestures.Wheel && progress > 0.5f)
-		{
-			string sGestureText = string.Format ("{0} detected, angle={1:F1} deg", gesture, screenPos.z);
-			if(gestureInfo != null)
+			if (gestureInfo != null)
 				gestureInfo.text = sGestureText;
 
 			progressDisplayed = true;
 		}
 	}
 
-	public bool GestureCompleted (uint userId, int userIndex, KinectGestures.Gestures gesture,
-	                              KinectWrapper.NuiSkeletonPositionIndex joint, Vector3 screenPos)
+	public bool GestureCompleted(uint userId, int userIndex, KinectGestures.Gestures gesture,
+		KinectWrapper.NuiSkeletonPositionIndex joint, Vector3 screenPos)
 	{
-		string sGestureText = gesture + " detected";
-		if(gesture == KinectGestures.Gestures.Click)
-        {
-						planetClicked = !planetClicked;
+		string sGestureText = "";
+		if (gesture == KinectGestures.Gestures.Click)
+		{
+			
+			switch (planetClicked)
+			{
+			case true:
+				sGestureText = selectedPlanet.tag + " deposited.";
 
-            sGestureText += string.Format(" at ({0:F1}, {1:F1})", screenPos.x, screenPos.y);
-        }
+				planetClicked = false;
+				selectedPlanet = null;
+
+				break;
+			case false:
+				Vector2 rayPos = new Vector2(spaceship.transform.position.x, spaceship.transform.position.y);
+				RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+				if (hit)
+				{
+					Debug.Log(hit.transform.name);
+					Debug.Log(hit.transform.gameObject.tag);
+					planetClicked = true;
+					selectedPlanet = hit.transform.gameObject;
+
+					sGestureText = selectedPlanet.tag + " captured";
+				}
+				break;
+			}
+		}
 
 
 
-		if(gestureInfo != null)
-        {
-            gestureInfo.text = sGestureText;
-        }
-
+		if (gestureInfo != null)
+		{
+			gestureInfo.text = sGestureText;
+		}
 
 		progressDisplayed = false;
 
 		return true;
 	}
 
-	public bool GestureCancelled (uint userId, int userIndex, KinectGestures.Gestures gesture,
-	                              KinectWrapper.NuiSkeletonPositionIndex joint)
+	public bool GestureCancelled(uint userId, int userIndex, KinectGestures.Gestures gesture,
+		KinectWrapper.NuiSkeletonPositionIndex joint)
 	{
-		if(progressDisplayed)
+		if (progressDisplayed)
 		{
 			// clear the progress info
-			if(gestureInfo != null)
+			if (gestureInfo != null)
 				gestureInfo.text = String.Empty;
 
 			progressDisplayed = false;
@@ -123,9 +119,11 @@ public class SimpleGestureListener : MonoBehaviour, KinectGestures.GestureListen
 		return true;
 	}
 
-	void Update() {
-		if (planetClicked == true) {
-			jupiter.transform.position = spaceship.transform.position;
+	void Update()
+	{
+		if (planetClicked == true && selectedPlanet != null)
+		{
+			selectedPlanet.transform.position = spaceship.transform.position;
 		}
 	}
 }
