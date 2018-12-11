4806142e78ddc3c383cab305693912223bf87777
vluhadia <32604792+vluhadia@users.noreply.github.com>
2018-12-11 16:11:04 -0500

    initial ending positions

---
+++ Assets/updateScoreCount.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateScoreCount : MonoBehaviour {
+	private string currentScore;
+	public Text scoreText;
+
+	void Start () {
+		currentScore = scoreText.text;
+		scoreText.text = "Score: " + score.ToString ();
+
+
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y++;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y--;
+			this.transform.position = position;
+		}
+
+
+
+
+
+
+	}
+	void OnCollisionEnter(Collision col) {
+		if(col.gameObject.name == "JupiterEnding") {
+			System.Console.WriteLine ("plus10");
+
+		}
+
+
+
+
+	}
+}
