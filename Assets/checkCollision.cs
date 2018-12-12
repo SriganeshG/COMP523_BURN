3ac264382f1aa79ba9f125cb13c5d80450e1e8f6
vluhadia <32604792+vluhadia@users.noreply.github.com>
2018-12-11 19:16:43 -0500

    ending postions

---
+++ Assets/checkCollision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider other) {
+		Debug.Log ("detect");
+	}
+}
